327ea1cb0d6b1cc0893c28bbf12458a4136d10cc
Daniel Franze <daniel.franze@outlook.de>
2024-04-05 18:20:21 +0200

    [test] Remove useless test annotation.

--- src/test/kotlin/SortingAlgorithmTester.kt
+++ src/test/kotlin/SortingAlgorithmTester.kt
@@ -1,11 +1,10 @@
 import com.franze.playground.algorithm.SortingAlgorithm
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+
 
 class SortingAlgorithmTester(private val sortingAlgorithm: SortingAlgorithm) {
 
-    @Test
     fun testSort() {
         val inputList = mutableListOf(12, 14, 15, 5, 17, 11, 19, 1, 18, 2, 10, 6, 9, 13, 20, 4, 16, 8, 7, 3)
         val targetList = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
