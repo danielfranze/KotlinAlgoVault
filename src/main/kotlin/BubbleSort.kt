179e67e391d7dd5d1cf06d9282d4724b5ed30983
Daniel Franze <daniel.franze@outlook.de>
2024-04-05 14:47:47 +0200

    [fix] Fix a bug that occurred after the implementation of swap in BubbleSort.

--- src/main/kotlin/BubbleSort.kt
+++ src/main/kotlin/BubbleSort.kt
@@ -11,22 +11,22 @@ import com.franze.playground.algorithm.util.swap
  * average case : O(n²)
  *
  * @author Daniel Franze
- * @version 1.0
+ * @version 1.1
  * @since 2024-03-29
  */
 class BubbleSort : SortingAlgorithm {
     override fun <T> sort(unsortedList: List<T>): MutableList<T> where T : Number, T : Comparable<T> {
         val list = unsortedList.toMutableList() // generate a depth copy
 
-        var i: Int = list.size - 1
+        var i: Int = list.size - 1 // unsorted index
         var swap = true // to abort the algorithm if no value was swapped in an iteration
 
         while (i >= 0 && swap) {
             swap = false
-            for (j in 0 until i) {
+            for (j in 0 until i ) {
                 // swap the values if j is greater than j+1
-                if (j + 1 <= i && list.elementAt(j) > list.elementAt(j + 1)) {
-                    list.swap(i, j)
+                if(list.elementAt(j) > list.elementAt(j+1)){
+                    list.swap(j, j+1)
                     swap = true
                 }
             }
