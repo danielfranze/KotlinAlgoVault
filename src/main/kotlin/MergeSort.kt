5d06fbf6278b346f99c9102f9ac9edee549d2c16
Daniel Franze <daniel.franze@outlook.de>
2024-04-07 10:43:53 +0200

    [refactor] Improve readability of the code in MergeSort.

--- src/main/kotlin/MergeSort.kt
+++ src/main/kotlin/MergeSort.kt
@@ -15,37 +15,35 @@ package com.franze.playground.algorithm
 class MergeSort : SortingAlgorithm {
 
     private fun <T> merge(leftList: MutableList<T>, rightList: MutableList<T>): MutableList<T> where T : Number, T : Comparable<T> {
-        val mergedList = mutableListOf<T>() // generate a depth copy
-
         if (rightList.isEmpty()) {
             return leftList
         } else if (leftList.isEmpty()) {
             return rightList
         }
-
-        //println("left: $leftList  right: $rightList")
+        // conquer part
+        val mergedList = mutableListOf<T>()
+        // find the smallest element and move to the end of mergedList
         while (leftList.isNotEmpty() || rightList.isNotEmpty()) {
-            if (leftList.isNotEmpty() && (rightList.isEmpty() || leftList[0] < rightList[0])) {
-                mergedList.addLast(leftList[0])
-                leftList.removeAt(0)
+            if (leftList.isNotEmpty() && (rightList.isEmpty() || leftList.first() < rightList.first())) {
+                mergedList.addLast(leftList.first())
+                leftList.removeFirst()
             } else if (rightList.isNotEmpty()) {
-                mergedList.addLast(rightList[0])
-                rightList.removeAt(0)
+                mergedList.addLast(rightList.first())
+                rightList.removeFirst()
             }
         }
-
         return (mergedList)
     }
 
     override fun <T> sort(unsortedList: List<T>): MutableList<T> where T : Number, T : Comparable<T> {
         val list = unsortedList.toMutableList() // generate a depth copy
-
-        var sortedList = mutableListOf<T>()
         if (list.size == 1) {
             return list
         }
 
+        var sortedList = mutableListOf<T>()
         while (sortedList.isEmpty()) {
+            // divide part
             val leftSide = list.slice(0..(list.size / 2 - 1))
             val rightSide = list.slice((list.size / 2)..list.size - 1)
 
@@ -56,7 +54,6 @@ class MergeSort : SortingAlgorithm {
                 sortedList = merge(sort(rightSide), sortedList)
             }
         }
-
         return sortedList
     }
 }
